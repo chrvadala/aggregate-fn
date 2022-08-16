97ffabb2499e93659dddbaa530e89fed1d783e18
Christian Vadalà <chr.vadala@gmail.com>
2022-08-16 20:01:46 +0200

    adds example

---
+++ example.js
@@ -0,0 +1,40 @@
+import { aggregateFn } from './src/aggregateFn.js'
+import { promisify } from 'util'
+const sleep = promisify(setTimeout)
+
+// this is a way to gather metrics
+const statsPrinter = stats => console.log(`Stats: ${JSON.stringify(stats)}`)
+
+// Here you can execute your async task (e.g. call one API)
+const myAggregableAsyncFn = async requests => {
+  const responses = requests.map(request => request * 2)
+  return responses
+}
+
+const { fn, flush, cancel } = aggregateFn(myAggregableAsyncFn, { maxWait: 200, maxTasks: 2, stats: statsPrinter })
+
+Promise.all([
+  (async function () {
+    const res = await fn(1)
+    console.log(`Request #1: ${res}`)
+  }()),
+
+  (async function () {
+    const res = await fn(2)
+    console.log(`Request #2: ${res}`)
+  }()),
+
+  (async function () {
+    const res = await fn(3)
+    console.log(`Request #3: ${res}`)
+  }()),
+
+  (async function () {
+    await sleep(500)
+    const res = await fn(4)
+    console.log(`Request #4: ${res}`)
+  }())
+])
+
+process.on('SIGINT', () => flush())
+process.on('exit', () => cancel())
