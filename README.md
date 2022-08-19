90a41e8c1f655d3b7d4cc0afd38e06c66a5b1e0b
Christian Vadalà <chr.vadala@gmail.com>
2022-08-19 18:46:26 +0200

    adds documentation

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # aggregate-fn
-aggregate-fn
+
+Aggregate fn is a Javascript utility that brings together multiple async operations. It is really useful when you want to merge many API requests together.
+
+[![chrvadala](https://img.shields.io/badge/website-chrvadala-orange.svg)](https://chrvadala.github.io)
+[![Test](https://github.com/chrvadala/aggregate-fn/workflows/Test/badge.svg)](https://github.com/chrvadala/node-ble/actions)
+[![Coverage Status](https://coveralls.io/repos/github/chrvadala/aggregate-fn/badge.svg)](https://coveralls.io/github/chrvadala/aggregate-fn)
+[![npm](https://img.shields.io/npm/v/aggregate-fn.svg?maxAge=2592000?style=plastic)](https://www.npmjs.com/package/aggregate-fn)
+[![Downloads](https://img.shields.io/npm/dm/aggregate-fn.svg)](https://www.npmjs.com/package/aggregate-fn)
+[![Donate](https://img.shields.io/badge/donate-PayPal-green.svg)](https://www.paypal.me/chrvadala/25)
+
+# Features 
+This utility can:
+- aggregate requests up to a configured upper limit
+- aggregate requests limiting the delay 
+- provide you interesting metrics
+
+# Documentation
+- [APIs](https://github.com/chrvadala/aggregate-fn/blob/main/docs/api.md)
+
+# Install
+```sh
+npm install aggregate-fn
+```
+# Examples
+````js
+import { aggregateFn } from 'aggregate-fn'
+import { promisify } from 'util'
+const sleep = promisify(setTimeout)
+
+// this is a way to gather metrics
+const statsPrinter = stats => console.log(`Stats: ${JSON.stringify(stats)}`)
+
+// Here you can execute your async task (e.g. call one API)
+const myAggregableAsyncFn = async requests => {
+  const responses = requests.map(request => request * 2)
+  return responses
+}
+
+const { fn, flush, cancel } = aggregateFn(myAggregableAsyncFn, { 
+  maxWait: 200, 
+  maxTasks: 2, 
+  stats: statsPrinter
+})
+
+Promise.all([ // let's try with some parallelism
+  (async function () {
+    const res = await fn(1)
+    console.log(`Request #1: ${res}`)
+  }()),
+
+  (async function () {
+    const res = await fn(2)
+    console.log(`Request #2: ${res}`)
+  }()),
+
+  (async function () {
+    const res = await fn(3)
+    console.log(`Request #3: ${res}`)
+  }()),
+
+  (async function () {
+    await sleep(500)
+    const res = await fn(4)
+    console.log(`Request #4: ${res}`)
+  }())
+])
+
+process.on('SIGINT', () => flush())
+process.on('exit', () => cancel())
+````
+
+# Changelog
+- **0.x** - Beta version
+- **1.0** - First official version
+# Contributors
+- [chrvadala](https://github.com/chrvadala) (author)
+
+<hr>
+<sub>In Memory of My Love :tulip:</sub>
--- src/aggregateFn.js
+++ src/aggregateFn.js
@@ -1,10 +1,27 @@
+/**
+   * Creates a wrapper of the provided function that enables aggregation capabilities
+   * @function aggregateFn
+   * @param {function} [fn] - Async function that has to be wrapped
+   * @param {object} [options] - Configuration
+   * @param {number} [options.maxWait = 100] - Max ms that the first request in queue can wait
+   * @param {number} [options.maxTasks = 100] - Max number of requests that can be aggregated together
+   * @param {statsCb} [options.stats] -  Callback called every time that a requested is served.
+   * @returns {aggregateFnReturnObject}
+   *
+   * @example
+   * import { aggregateFn } from 'aggregateFn
+   * const { fn, flush, cancel } = aggregateFn(myAggregableAsyncFn, {
+   *   maxWait: 200,
+   *   maxTasks: 2,
+   *   stats: console.log
+   * })
+   */
+
 export function aggregateFn (fn, { maxWait = 100, maxTasks = 100, stats = null }) {
   let accumulator = [] // { params, resolve, reject}
   let timer = null
 
-  /**
-   * Forces to call fn() and execute any accumulated request
-   */
+  // Forces to call fn() and execute any accumulated request
   const flush = async () => {
     const _acc = accumulator
     accumulator = []
@@ -34,9 +51,7 @@ export function aggregateFn (fn, { maxWait = 100, maxTasks = 100, stats = null }
     }
   }
 
-  /**
-   * Discard any accumulated request
-   */
+  // Discard any accumulated request
   const cancel = () => {
     clearTimeout(timer)
     const _acc = accumulator
@@ -47,12 +62,7 @@ export function aggregateFn (fn, { maxWait = 100, maxTasks = 100, stats = null }
     }
   }
 
-  /**
-   * Accumulate a single request
-   * @param {*} params
-   * @returns
-   */
-
+  // Accumulate a single request
   const wrappedFn = (...params) => {
     let resolveTask, rejectTask
     const promise = new Promise((resolve, reject) => {
@@ -82,3 +92,19 @@ export function aggregateFn (fn, { maxWait = 100, maxTasks = 100, stats = null }
 
   return { fn: wrappedFn, flush, cancel }
 }
+
+/**
+ * @typedef aggregateFnReturnObject
+ * @property {function} [fn] - Wrapped version of the provided function
+ * @property {function} [flush] - A function that forces to call immediately the original function regardless of maxWait and maxTasks configuration
+ * @property {function} [cancel] - A function that cancels any pending request
+ */
+
+/**
+ * This callback is displayed as part of the Requester class.
+ * @callback statsCb
+ * @param {boolean} ok - Indicated if the requested has been completed with success
+ * @param {number} count - Number of aggregated requests
+ * @param {number} swarf - Indicates the wasted aggregation (maxTasks - count)
+ * @param {number} delay - Delay introduced to the first queued request
+ */
